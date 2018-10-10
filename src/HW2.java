031d4319e21a12de80885aed5b0aa08b5fbf1628
Liam Donohoe <donohl@rpi.edu>
2018-10-09 22:45:35 -0400

    Begun the programming

---
+++ src/HW2.java
@@ -0,0 +1,22 @@
+/**
+ * Implementation of the Needham-Schroeder protocol, using Computational Diffie-Helman key exchange protocol
+ * @author donohl
+ *
+ */
+public class HW2 {
+
+	private static String key_dist() {
+		String packet = "Hello";
+		
+		
+		
+		return packet;
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
